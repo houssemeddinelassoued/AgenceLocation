94d4dced38e2fafc66752d29db67b9049d950875
Houssem Eddine Lassoued <houssemeddine.lassoued@esprit.tn>
2022-11-08 14:44:26 +0100

    update ClassA

--- ClassA.java
+++ ClassA.java
@@ -1,13 +1,13 @@
 import java.lang.*;//par houssem en ligne
 
 import java.sql.*;//par Bob 22
-public class ClassA2{
+public class ClassA{
 	
 	public String meth1(){
 
 		//TODO 1 compléter la méthode
 
-		System.err.println("Hello Git/Github");
+		System.out.println("Hello Git/Github");
 		return "";
 	}
 }
